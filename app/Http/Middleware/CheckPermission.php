2c7b450c5675842d50bdfba54097d6c3e56557a7
Manar <manarjpr@gmail.com>
2026-07-29 11:59:05 +0300

    Add permission middleware

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckPermission
+{
+    public function handle(Request $request, Closure $next, string $permission): Response
+    {
+        $user = $request->user();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Unauthenticated'
+            ], 401);
+        }
+
+        $hasPermission = $user->role
+            ? $user->role
+                ->permissions
+                ->contains('name', $permission)
+            : false;
+
+
+        if (!$hasPermission) {
+            return response()->json([
+                'message' => 'Forbidden'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
